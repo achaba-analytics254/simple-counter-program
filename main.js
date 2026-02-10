346079f6901ecd4a5f75adffc96367af1d0db0f9
achaba-analytics254 <djangozent@gmail.com>
2026-02-10 17:02:49 +0300

    Update: counter program

---
+++ main.js
@@ -0,0 +1,27 @@
+const decreaseBtn = document.getElementById('decreaseBtn');
+const resetBtn = document.getElementById('resetBtn');
+const increaseBtn = document.getElementById('increaseBtn');
+const counterLabel = document.getElementById('counterLabel');
+
+let count = 0;
+
+if (decreaseBtn){
+    decreaseBtn.addEventListener('click', () => {
+        count--;
+        counterLabel.textContent = count;
+    });
+}
+
+if (resetBtn){
+    resetBtn.addEventListener('click', () => {
+        count = 0;
+        counterLabel.textContent = count;
+    })
+}
+
+if (increaseBtn){
+    increaseBtn.addEventListener('click', () => {
+        count--;
+        counterLabel.textContent = count;
+    })
+}
